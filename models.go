11ebea3ce2692b1e0fa8e886d8f8f888f3495ed2
RubenAbehsera <ruben.abehsera@eemi.com>
2022-11-01 16:55:02 +0100

    first commit, install project and connect database (sqlite)

---
+++ models.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+type Movie struct {
+	ID          int64  `db:"id"`
+	Title       int64  `db:"title"`
+	ReleaseDate string `db:"release_date"`
+	Duration    int    `db:"duration"`
+	TrailerUrl  string `db:"trailer_url"`
+}
+
+func (m Movie) String() string {
+	return fmt.Sprintf("id=%v, title=%v, releaseDate=%v, duration=%v, trailerUrl=%v", m.ID, m.Title, m.ReleaseDate, m.Duration, m.TrailerUrl)
+}
